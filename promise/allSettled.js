95b3dabc2cc2d1abf74a1780c2de85bdd697f3df
Poulami Saha <saha.poulami91@gmail.com>
2024-01-31 09:22:31 +0100

    minor corrections in Promise.AllSettled

--- promise/allSettled.js
+++ promise/allSettled.js
@@ -1,9 +1,10 @@
-//MDN definition of Promise.Race
+//MDN definition of Promise.AllSettled
 //The Promise.allSettled() static method takes an iterable of promises as input and returns a single Promise.
 //This returned promise fulfills when all of the input's promises settle (including when an empty iterable is passed),
 //with an array of objects that describe the outcome of each promise.
 
 //Requirements
+//Returns a new Promise
 //Map the array of promises to return an object with status and value/error depending upon the promised settlement.
 //Returned promise resolves with the value of the input promise or rejects with the reason of the input promise
 
